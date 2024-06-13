8f44619478cc9455c1a983db6b3c1dfd0948ff71
Ashen Rashmika <105934940+ashenrashmike2000@users.noreply.github.com>
2024-06-13 20:13:02 +0000

    Finish the Programming Foundations: Algorithms Course

--- Start/DataStructures/Dictionary/dictionary_start.py
+++ Start/DataStructures/Dictionary/dictionary_start.py
@@ -3,15 +3,23 @@
 
 
 # TODO: create a dictionary all at once
-
+itmesl = dict({"key1" : 1, "key2" : 2, "key3" : 3})
+print(itmesl)
 
 # TODO: create a dictionary progressively
-
+items2 = {}
+items2["key1"] = 1
+items2["key2"] = 2
+items2["key3"] = 3
+print(items2)
 
 # TODO: replace an item
-
+items2["key2"] = "two"
+print(items2)
 
 # TODO: try to access a nonexistent key
-
+# print(itmesl["key6"])
 
 # TODO: iterate the keys and values in the dictionary
+for key, value in items2.items():
+  print("Key:",key," value: ",value)
